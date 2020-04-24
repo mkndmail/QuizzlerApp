37ee4ad3232e9a67673bba7ea6cb2db0ca905ffe
Mukund <mkndmail@gmail.com>
2020-04-24 18:14:26 +0530

    Quizzler app

---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestions().question,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  setTrueFalse(true, quizBrain.getQuestions().answer);
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  setTrueFalse(false, quizBrain.getQuestions().answer);
+                });
+              },
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Row(
+            children: scoreKeeper,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Icon setScoreIcon({IconData icon, Color color}) {
+    return Icon(
+      icon,
+      color: color,
+    );
+  }
+
+  void setTrueFalse(bool userAnswer, bool answer) {
+    setState(() {
+      if (quizBrain.isFinished()) {
+        scoreKeeper.clear();
+        Alert(
+                context: context,
+                title: "Quiz End",
+                desc: "You have reached the end of the quiz.")
+            .show();
+        quizBrain.reset();
+      } else {
+        if (userAnswer == answer) {
+          scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+        } else
+          scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+}
